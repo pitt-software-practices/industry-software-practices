0e77fe3529f0828ed429dcb3ae4b92cd1d37674a
Paul Boone <narcissus.pong@gmail.com>
2020-08-31 10:29:42 -0400

    addding python3-venv requirement which was missing for ubuntu dev env to work

--- module-dev-env/ubuntu-setup.sh
+++ module-dev-env/ubuntu-setup.sh
@@ -11,4 +11,4 @@ sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any m
 apt-get update
 
 apt-get install -y atom vim
-apt-get install -y build-essential python3 python3-pip
+apt-get install -y build-essential python3 python3-pip python3-venv
--- module-dev-env/wsl-ubuntu.sh
+++ module-dev-env/wsl-ubuntu.sh
@@ -3,7 +3,7 @@ set -ex
 
 apt-get update
 apt-get install -y curl vim
-apt-get install -y build-essential python3 python3-pip
+apt-get install -y build-essential python3 python3-pip python3-venv
 
 
 # install atom: https://atom.io/
