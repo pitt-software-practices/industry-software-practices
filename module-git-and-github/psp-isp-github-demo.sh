6456d73cd7a1fb41b53ee4c6f647caa963e1c7cc
Paul Boone <narcissus.pong@gmail.com>
2020-09-05 12:43:20 -0400

    separate doc for exercise with pitt-software-practices/industry-software-practices repo

--- module-git-and-github/psp-isp-github-demo.sh
+++ module-git-and-github/psp-isp-github-demo.sh
@@ -1,20 +1,20 @@
 # First:
-# 1. send link to your github to me so I can fork it to github.com/pitt-software-practices
-# 2. set up your github SSH key! https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+# 1. set up your github SSH key! https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 
 # Quick intro to git: https://levelup.gitconnected.com/what-is-git-how-to-use-it-why-to-use-it-explained-in-depth-76a5066abaaa
 
 
-# Note: if your repo is already local, some of these steps are unnecessary
 
-git clone {your repo here - USE SSH link}
+# fork the psp repo (https://github.com/pitt-software-practices/industry-software-practices) into your github account
+
+git clone {your fork here - USE SSH link}
 # git clone sets up a remote named "origin" to repo you specify
 
 cd {your repo}
 
 # Now lets add a new remote for pitt software practices
 # psp is for "Pitt Software Practices"
-git remote add psp {PSP fork of your repo here - either SSH or https link}
+git remote add psp https://github.com/pitt-software-practices/industry-software-practices.git
 
 git fetch psp
 # this syncs all code from the PSP repo to your local repository
